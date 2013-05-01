ae1cfb04e78aec317f44c223a12e4b31a6f3a278
Jerad Gallinger <jerad@jeradgallinger.ca>
2013-05-01 08:27:59 -0400

    Initial commit.

---
+++ jquery.fillwindow.js
@@ -0,0 +1,77 @@
+/*!
+* FillWindow.js 1.0
+*
+* Copyright 2013, Jerad Gallinger - http://jeradgallinger.ca
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+* Date: Wed May 01 8:00:00 2013 -0500
+*
+* A jQuery plugin that fits an element to the size of the document window.
+* Takes an optional parameter align, which can be a value of 'top' or 'bottom'.
+* If align === 'top', fillWindow assigns padding only to the bottom of the element.
+* If align === 'bottom', fillWindow assigns padding only to the top of the element.
+* By default, fillWindow assigns equal padding to the top and bottom of the element.
+
+*/
+
+( function ( $ ) { 
+  
+  "use strict";
+
+  $.fn.fillWindow = function( options ) {
+    var settings = {
+          align: null
+        },
+        $this,
+        height,
+        newPadding,
+        paddingTop,
+        paddingBottom,
+        thisTotalHeight,
+        windowHeight = parseInt( $( window ).height(), 10 );
+    
+    function paddingAmount() {
+      if ( ( settings.align === 'top' ) || ( settings.align === 'bottom' ) ) {
+        return Math.ceil( windowHeight - height ) + 'px';
+      } else {
+        return Math.ceil( ( windowHeight - height ) / 2 ) + 'px';
+      }
+    }
+
+    function assignPadding( $element ) {
+      // If the window is taller than the element, pad the element so that it is as tall as the window.
+      // Otherwise, make the element's padding equal 0.
+      newPadding = ( windowHeight > height ) ? paddingAmount() : 0;
+      if ( settings.align === 'top' ) {
+        $element.css( 'padding-bottom', newPadding );
+      } else if ( settings.align === 'bottom' ) {
+        $element.css( 'padding-top', newPadding );
+      } else {
+        $element.css( 'padding-top', newPadding );
+        $element.css( 'padding-bottom', newPadding );
+      }
+    }
+
+    if ( options ) {
+      $.extend( settings, options );
+    }
+
+    this.each( function() {
+      $this = $( this );
+      height = parseInt( $this.height(), 10 );
+      paddingTop = parseInt( $this.css( 'padding-top' ), 10 );
+      paddingBottom = parseInt( $this.css( 'padding-bottom' ), 10 );
+      thisTotalHeight = height
+                      + paddingTop
+                      + paddingBottom;
+
+      // If the element is shorter than the window, fit the element to the window;
+      // OR if the element is taller than the window and has padding-top or padding-bottom,
+      // remove the padding-top and -bottom from the element
+      if ( ( thisTotalHeight < windowHeight ) || ( ( thisTotalHeight > windowHeight ) && ( ( paddingTop !== 0 ) || ( paddingBottom !== 0 ) ) ) ) {
+        assignPadding( $this );
+      }
+    } );
+
+  }
+} )( jQuery );
---
+++ readme.md
@@ -0,0 +1,29 @@
+# FillWindow.js
+
+A simple jQuery plugin to make a website section fill the browser window.
+
+## How do I use it?
+
+Include jQuery and FillWindow.js in your webpage. Target an element or elements with ``fillWindow()``. The elements that are shorter than the window's height will be resized to the height of the window on page load. (Elements that are already taller than the window won't be affected.)
+
+## Making it responsive
+
+You can easily add a basic level of responsiveness by binding fillWindow to the window's resize event:
+
+```javascript
+$( window ).resize( $( '.element' ).fillWindow() );
+```
+
+## Options
+
+By default, ``fillWindow()`` adds equal padding to the top and bottom of the target element to fit it to the window. ``fillWindow()`` takes an optional ``options`` argument. For now the only option is ``align``, which can have a value of ``'top'`` or ``'bottom'``. If ``align`` is set to ``'top'``, all of the padding is added to the bottom of the element, which pushes content to the top of the element. ``'bottom'`` does the opposite, pushing the content to the bottom of the element.
+
+Example usage:
+
+```javascript
+$( '.element' ).fillWindow( { align: 'bottom' } );
+```
+
+##TODO
+- [ ] Add option "resize" that makes element resize to fill the window on browser resize.
+- [ ] Make it work on mobile devices.
